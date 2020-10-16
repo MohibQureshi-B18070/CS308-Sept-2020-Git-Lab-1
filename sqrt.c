75eaa17a5b30e09144b716f769509ed9cb33d295
mohibqureshi <mohibqureshi2@gmail.com>
2020-10-16 18:15:28 +0530

    added negative support

--- sqrt.c
+++ sqrt.c
@@ -12,20 +12,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Usage: sqrt input\n\n");
+		printf("NEGSUPPORT: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if (input >= 0)
 	{
-		printf("Sqrt of %d is %f\n", input, sqrt(input));
-		printf("End of program.Exiting.\n");
+		printf("NEGSUPPORT: Sqrt of %d is %f\n", input, sqrt(input));
+		printf("NEGSUPPORT: End of program.Exiting.\n");
 	}
 
 	else
 	{
-		printf("input is not valid");
+		printf("NEGSUPPORT: input is not valid");
 	}
 
 	return (0);
