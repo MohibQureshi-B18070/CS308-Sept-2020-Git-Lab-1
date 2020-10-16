3a8457d16decfdfe15963e50fc266f6887b76d89
mohibqureshi <mohibqureshi2@gmail.com>
2020-10-16 18:17:39 +0530

    added negative support(lowercase)

--- sqrt.c
+++ sqrt.c
@@ -12,20 +12,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("NEGSUPPORT: Usage: sqrt input\n\n");
+		printf("negsupport: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if (input >= 0)
 	{
-		printf("NEGSUPPORT: Sqrt of %d is %f\n", input, sqrt(input));
-		printf("NEGSUPPORT: End of program.Exiting.\n");
+		printf("negsupport: Sqrt of %d is %f\n", input, sqrt(input));
+		printf("negsupport: End of program.Exiting.\n");
 	}
 
 	else
 	{
-		printf("NEGSUPPORT: input is not valid");
+		printf("negsupport: input is not valid");
 	}
 
 	return (0);
